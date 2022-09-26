479ba7703ad7a2fb4299617f0165e5c8320202c8
jc-development <joe.cardella3@gmail.com>
2022-09-26 08:26:03 -0400

    refactored and split out into functions and recursion

--- src/lib.rs
+++ src/lib.rs
@@ -21,15 +21,39 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.move_to(300.0, 0.0); // top of triangle
-    context.begin_path();
-    context.line_to(0.0, 600.0); // bottom left of triangle
-    context.line_to(600.0, 600.0); // bottom right of triangle
-    context.line_to(300.0, 0.0); // back to top of triangle
-
-    context.close_path();
-    context.stroke();
-    context.fill();
+    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 5);
 
     Ok(())
+}
+
+fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], depth: u8) {
+    draw_triangle(&context, points);
+
+    let depth = depth - 1;
+    let [top, left, right] = points;
+
+    if depth > 0 {
+        let left_middle = midpoint(top, left);
+        let right_middle = midpoint(top, right);
+        let bottom_middle = midpoint(left, right);
+
+        sierpinski(&context, [top, left_middle, right_middle], depth);
+        sierpinski(&context, [left_middle, left, bottom_middle], depth);
+        sierpinski(&context, [right_middle, bottom_middle, right], depth);
+    }
+}
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
 }
