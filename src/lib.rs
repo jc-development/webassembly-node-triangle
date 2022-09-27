47ebd42f2e34b6f21b2d51b0918a4361daf3ecd3
jc-development <joe.cardella3@gmail.com>
2022-09-27 08:28:02 -0400

    added sprite image

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,25 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        5,
-    );
+    let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+    wasm_bindgen_futures::spawn_local(async move {
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            success_tx.send(());
+        });
 
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_src("Idle (1).png");
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            5,
+        );
+    });
     Ok(())
 }
 
