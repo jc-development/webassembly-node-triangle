668f670e1d4ef67ee344df17ded1d7791057d2f7
jc-development <joe.cardella3@gmail.com>
2022-09-29 08:17:56 -0400

    added Option passed in transmitter with RC; now oneshot is locked and threadsafe, the Mutex always contains Some or None

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use rand::prelude::*;
+use std::rc::Rc;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -22,14 +24,28 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+    let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+
+    let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+    let error_tx = Rc::clone(&success_tx);
+
     wasm_bindgen_futures::spawn_local(async move {
         let image = web_sys::HtmlImageElement::new().unwrap();
+
         let callback = Closure::once(move || {
-            success_tx.send(());
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
         });
 
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("Idle (1).png");
         success_rx.await;
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
