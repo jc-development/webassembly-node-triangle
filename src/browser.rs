e92e1abc7f56f49f680fc17b614b104e214640c9
jc-development <58231556+jc-development@users.noreply.github.com>
2022-10-28 09:04:51 -0400

    Update browser.rs

--- src/browser.rs
+++ src/browser.rs
@@ -1,5 +1,8 @@
 use anyhow::{anyhow, Result};
-use web_sys::{Document, Window};
+use futures::Future;
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Response, Window};
 
 // macro that allows you to log to the console with log!
 macro_rules! log {
@@ -17,4 +20,53 @@ pub fn document() -> Result<Document> {
     window()?
         .document()
         .ok_or_else(|| anyhow!("No Document Found"))
+}
+
+pub fn canvas() -> Result<HtmlCanvasElement> {
+    document()?
+        .get_element_by_id("canvas")
+        .ok_or_else(|| anyhow!("No Canvas Found with ID 'canvas'"))?
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|element| anyhow!("Error converting {:#?} to HtmlCanvasElement", element))
+}
+
+pub fn context() -> Result<CanvasRenderingContext2d> {
+    canvas()?
+        .get_context("2d")
+        .map_err(|js_value| anyhow!("Error getting 2d context {:#?}", js_value))?
+        .ok_or_else(|| anyhow!("No 2d context found"))?
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .map_err(|element| {
+            anyhow!(
+                "Error converting {:#?} to CanvasRenderingContext2d",
+                element
+            )
+        })
+}
+
+pub fn spawn_local<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future);
+}
+
+pub async fn fetch_with_str(resource: &str) -> Result<JsValue> {
+    JsFuture::from(window()?.fetch_with_str(resource))
+        .await
+        .map_err(|err| anyhow!("Error fetching {:#?}", err))
+}
+
+pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
+    let resp_value = fetch_with_str(json_path).await?;
+    let resp: Response = resp_value
+        .dyn_into()
+        .map_err(|element| anyhow!("Error converting {:#?} to Response", element))?;
+
+    JsFuture::from(
+        resp.json()
+            .map_err(|err| anyhow!("Could not get JSON from response {:#?}", err))?,
+    )
+    .await
+    .map_err(|err| anyhow!("Error fetching JSON {:#?}", err))
 }
